28251ea16fdafcd92ea288d1eab0691862d02425
Yeech <yzhu44@illinois.edu>
2018-03-30 02:39:48 -0500

    add draft for constructing tree, need quick select and worker data ingestion to be implemented

---
+++ helper.go
@@ -0,0 +1,13 @@
+package geocube
+
+func argmax(values []float64) int {
+	max_v = values[0]
+	max_ind = 0
+	for i, v := range values {
+		if v > max_v {
+			max_v = v
+			max_ind = i
+		}
+	}
+	return max_ind
+}
---
+++ tree.go
@@ -0,0 +1,197 @@
+// Copyright (c) 2018 The geocube Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package geocube
+
+import (
+	"math"
+	"errors"
+	"fmt"
+)
+
+const defaultBufSize = 128
+
+type Cube struct {
+}
+
+type DTreeNode struct {
+	isLeaf bool
+	//parent,left,right node index
+	//pInd uint
+	lInd uint
+	rInd uint
+
+	//min max for each dimension
+	mins  []float64
+	maxs  []float64
+	cells []float64
+
+	//capacity in each node for each dim
+	//depends on the cache design, dimension can be sorted by priority
+	//least important data comes first
+	dims  []uint
+	dCaps []uint
+
+	currNum uint
+
+	splitDim uint
+	splitVal float64
+}
+
+func (node *DTreeNode) initTreeNode(mins, maxs, dims, dCaps) {
+
+	node.mins = make([]float64, len(mins))
+	node.maxs = make([]float64, len(maxs))
+	node.dims = make([]uint, len(dims))
+	node.dCaps = make([]uint, len(dCaps))
+
+	node.cells = make([]float64, len(node.mins))
+	for i, c := range node.dCaps {
+		node.cells[i] = (node.maxs[i] - node.mins[i]) / c
+	}
+	node.isLeaf = true
+}
+
+func (node *DTreeNode) checkRange(point *DataPoint) error {
+	err := nil
+	for i, d := range node.dims {
+		v := point.getByDim(d)
+		if v < node.mins[i] {
+			err := errors.New(fmt.Sprintf("Data has value %d on dim %d, exceeds minimum", v, d))
+			fmt.Println(err)
+		} else if v > node.maxs[i] {
+			err := errors.New(fmt.Sprintf("Data has value %d on dim %d, exceeds minimum", v, d))
+			fmt.Println(err)
+		}
+	}
+	return err
+}
+
+func (node *DTreeNode) MapInd(point *DataPoint) uint {
+	mapInd1d := func(x, xmin, cell float64) int {
+		//fmt.Println("")
+		return int(math.Floor((x - xmin) / cell))
+	}
+
+	ind := 0
+	for i, d := range node.dims {
+		v := point.getByDim(d)
+		ind *= node.dCaps[i]
+		ind += mapInd1d(v, node.mins[i], node.cells[i])
+	}
+	return ind
+}
+
+type DTree struct {
+	nodes    []DTreeNode
+	nodeData [][]DataPoint
+	dims     []uint
+	//capacity in each node for each dim
+	dCaps      []uint
+	capacity   uint
+	splitThres uint
+
+	warnings []string
+}
+
+// Initialize the DTree structure, must be called after declaration
+func (dTree *DTree) initTree(pDims []uint, pCaps []uint, splitThresRatio float64) error {
+	dTree.dims = make([]uint, len(pDims))
+	dTree.dims[:] = pDims[:]
+
+	dTree.pCaps = make([]uint, len(pCaps))
+	dTree.pCaps[:] = pCaps[:]
+
+	dTree.capacity = 1
+	for i,c := pCaps{
+		dTree.capacity *= c
+	}
+	dTree.splitThres = uint(math.Floor(dTree.capacity * splitThresRatio))
+
+	dTree.nodes = make([]DTreeNode, 1)
+	dTree.nodeData = make([][]DataPoint, 1)
+
+	dTree.nodes[0] = new(DTree)
+	dTree.nodes[0].initTreeNode(dTree.mins, dTree.maxs, dTree.dims, dTree.dCaps)
+}
+
+// Assign single data point to the correct node 
+func (dTree *DTree) assignData(point *DataPoint, startNodeInd uint) error {
+	currNodeInd := uint(0)
+	if err := dTree.nodes[currNodeInd].checkRange(point); err != nil{
+
+	}
+
+	//find leaf node
+	for dTree.nodes[currNodeInd].isLeaf == false {
+		v = point.getByDim(dTree.nodes[currNodeInd].splitDim)
+		if v < dTree.nodes[currNodeInd].splitVal {
+			currNodeInd = dTree.nodes[currNodeInd].lInd
+		} else {
+			currNodeInd = dTree.nodes[currNodeInd].rInd
+		}
+	}
+
+	if dTree.nodeData[currNodeInd] == nil {
+		dTree.nodeData[currNodeInd] = make([]DataPoint, 0)
+	}
+
+	if len(dTree.nodeData[currNodeInd]) == cap(dTree.nodeData[currNodeInd]){
+		dTree.nodeData[currNodeInd] = append(dTree.nodeData[currNodeInd], *point)
+	}else{
+		dTree.nodeData[len(dTree.nodeData[currNodeInd])] = *point
+	}
+	dTree.nodes[currNodeInd].currNum += 1
+
+	if dTree.nodes[currNodeInd].currNum >= splitThres{
+		err := dTree.splitLeaf(currNodeInd)
+		if err != nil{
+			return err
+		}
+	}
+}
+
+
+// Split the specific node in Tree and update the Tree accordingly
+func (dTree *DTree) splitLeaf(splitNodeInd uint) error{
+	
+	if dTree.nodes[splitNodeInd].currNum < len(dTree.nodeData[splitNodeInd]){
+		//To do: acquire data from worker, currently WRONG
+		dTree.nodeData[splitNodeInd] = append(dTree.nodeData[splitNodeInd], dTree.nodeData[splitNodeInd])
+		if dTree.nodes[splitNodeInd].currNum != len(dTree.nodeData[splitNodeInd]){
+			err := errors.New(fmt.Sprintf("Incomplete data on node %d", splitNodeInd))
+			fmt.Println(err)
+			return err
+		}
+	}
+
+	dimCandidateValue := make(float64, len(dTree.dims))
+	dimCandidateMetric := make(float64, len(dTree.dims))
+	for j,d := range dTree.dims{
+		extractedData := make(float64, len(dTree.nodeData[splitNodeInd]))
+		for i,p := range dTree.nodeData[splitNodeInd]{
+			extractedData[i] = p.getByDim(d)
+		}
+		//To do: implement quickselect currently WRONG, shoud find real median
+		dimCandidateValue[j] = extractedData[uint(math.Floor(len(extractedData)/2))]
+
+		dimCandidateMetric[j] = math.Abs(dimCandidateValue[j]-(dTree.maxs[j]+dTree.mins[j])/2)/(dTree.maxs[j]-dTree.mins[j])
+	}
+
+	bestSplit = argmax(dimCandidateMetric)
+	dTree.nodes[splitNodeInd].splitDim = dTree.dims[bestSplit] 
+	dTree.nodes[splitNodeInd].splitThres = dimCandidateValue[bestSplit] 	
+
+	dTree.nodes = append(dTree.nodes, new(DTreeNode))
+	dTree.nodeData = append(dTree.nodeData, nil)
+	dTree.nodes[splitNodeInd].lInd := len(dTree.nodes)-1
+	dTree.nodes = append(dTree.nodes, new(DTreeNode))
+	dTree.nodeData = append(dTree.nodeData, nil)
+	dTree.nodes[splitNodeInd].rInd := len(dTree.nodes)-1	
+	dTree.nodes[splitNodeInd].isLeaf = false
+}
+
+// Batch update the tree assuming the tree has been loaded in the memory
+func (dTree *DTreeNode) updateTree(rows []DataPoint) error {
+
+}
